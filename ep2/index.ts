7cea051d4cf5570423c9ab7efb4b70df947d559d
Wutthipong Luangsanam <wutthipong.luangsanam@gmail.com>
2024-07-23 16:03:07 +0700

    เรียนฟังก์ชั่น

--- ep2/index.ts
+++ ep2/index.ts
@@ -11,7 +11,23 @@ function mul(a: number, b: number, c: number) {
 }
 
 
-function grade(homework: number, midterm: number, final: number){
+// function grade(homework: number, midterm: number, final: number){
+//     let score: number = homework + midterm + final
+
+//     if (score < 50) {
+//         return 'เกรด F'
+//     } else if (score < 60) {
+//         return 'เกรด D'
+//     } else if (score < 70) {
+//         return 'เกรด C'
+//     } else if (score < 80) {
+//         return 'เกรด B'
+//     }
+// }
+
+// console.log(grade(10, 20, 30))
+
+function grade(homework: number, midterm: number, final: number) {
     let score: number = homework + midterm + final
 
     if (score < 50) {
@@ -22,7 +38,12 @@ function grade(homework: number, midterm: number, final: number){
         return 'เกรด C'
     } else if (score < 80) {
         return 'เกรด B'
+    } else {
+        return 'เกรด A'
     }
 }
 
-console.log(grade(10, 20, 30))
+console.log(grade(100, 100, 100))
+
+
+
